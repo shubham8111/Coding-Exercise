793ba7e2d16ef8f92078c6c5637c19688aa689ce
Shubham <shubham8111@users.noreply.github.com>
2019-01-01 22:50:37 +0530

    intital
    Intial commit with readme and some exercises.

---
+++ anagrams.py
@@ -0,0 +1,83 @@
+'''
+Two strings are said to be anagrams of one another if you can turn the first string into
+the second by rearranging its letters. For example, "table" and "bleat" are anagrams, as
+are "tear" and "rate." Your job is to write a function that takes in two strings as input and
+determines whether they're anagrams of one another.
+
+Solution should run in O(n1 + n2)
+There are two solutions:
+	one is using counter
+	Another one is also using dictionary.
+'''
+"""
+New tests suggest one using counter is actually more than 2 times faster than one using dictionary
+"""
+from collections import Counter
+import random
+import string
+import time
+def are_anagrams(str1, str2):
+	if len(str1) != len(str2):
+		return False
+	dict1 = Counter(str1)
+	dict2 = Counter(str2)
+	for char in dict1:
+		if not char in dict2 or dict2[char] != dict1[char]:
+			return False
+	return True
+
+assert are_anagrams('table', 'bleat') == True
+assert are_anagrams('table', 'bleate') == False
+assert are_anagrams('honey', 'eyhon') == True
+assert are_anagrams('area', 'are') == False
+assert are_anagrams('', '') == True
+
+def are_anagrams_dict(str1, str2):
+	if len(str1) != len(str2):
+		return False
+
+	char_dict = {}
+	for char in str1:
+		if not char in char_dict:
+			char_dict[char] = 0
+		char_dict[char] += 1
+
+	for char in str2:
+		if char not in char_dict or char_dict[char] <= 0:
+			return False
+		char_dict[char] -= 1
+	return True
+
+assert are_anagrams_dict('table', 'bleat') == True
+assert are_anagrams_dict('table', 'bleate') == False
+assert are_anagrams_dict('honey', 'eyhon') == True
+assert are_anagrams_dict('area', 'are') == False
+assert are_anagrams_dict('', '') == True
+
+def time_test():
+	"""a time test to evaluate the performance two methods
+	results say anagrams_dict takes almost twice as much time :( """
+	loops = 10000
+	A = ""
+	B = ""
+	for x in range(1000):
+		A = A + random.choice(string.ascii_letters)
+	
+	A_shuff = list(A)
+	random.shuffle(A_shuff)
+	A_shuff = "".join(A_shuff)
+
+	s = time.clock()
+	for i in range(loops):
+		are_anagrams(A, A_shuff)
+	e = time.clock()
+	print("time by are_anagrams \t", e -s)
+	s = time.clock()
+	for i in range (loops):
+		are_anagrams_dict(A, A_shuff)
+	e = time.clock()
+	print("time by are_anagrams_dict\t", e -s)
+
+time_test()
+
+
